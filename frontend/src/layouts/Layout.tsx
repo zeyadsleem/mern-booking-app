e47cc9890f553613e46aa2ab327b3c079c2f7177
zeyadsleem <zeyadsleem1@gmail.com>
2024-02-26 23:04:26 +0200

    setup layoyt with tow routes , home and search

--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,9 +1,18 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
-export default function Layout() {
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Hero />
+      <div className="container mx-auto py-10 flex-1">{children}</div>
+      <Footer />
     </div>
   );
 }
